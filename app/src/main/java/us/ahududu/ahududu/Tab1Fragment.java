f7b7c544aa8892a023803c56ec4e63dc2073bafd
apple <donmezo17@itu.edu.tr>
2018-12-18 01:25:36 +0300

    Main Page Performance Problem Fixed

--- app/src/main/java/us/ahududu/ahududu/Tab1Fragment.java
+++ app/src/main/java/us/ahududu/ahududu/Tab1Fragment.java
@@ -29,50 +29,8 @@ public class Tab1Fragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.tab1_fragment,container,false);
-        System.gc();
-        ListView listView = view.findViewById(R.id.lvMainFlow);
-
-        CustomAdapter customAdapter = new CustomAdapter();
-        listView.setAdapter(customAdapter);
-
         return view;
     }
 
-    class CustomAdapter extends BaseAdapter {
-
-        @Override
-        public int getCount() {
-            return images.length;
-        }
-
-        @Override
-        public Object getItem(int i) {
-            return null;
-        }
-
-        @Override
-        public long getItemId(int i) {
-            return 0;
-        }
-
-        @Override
-        public View getView(int i, View view, ViewGroup viewGroup) {
-            view = getLayoutInflater().inflate(R.layout.customrow,null);
-
-            ImageView image = view.findViewById(R.id.imgFlow);
-            TextView txtName = view.findViewById(R.id.txtFlowName);
-            TextView txtCategories = view.findViewById(R.id.txtFlowCategories);
-            TextView txtPrices = view.findViewById(R.id.txtFlowPrices);
-            TextView txtRemainingTimes = view.findViewById(R.id.txtFlowRemaining);
-
-            image.setImageResource(images[i]);
-            txtName.setText(names[i]);
-            txtCategories.setText(categories[i]);
-            txtPrices.setText(prices[i]);
-            txtRemainingTimes.setText(remainingTimes[i]);
-
-            return null;
-        }
-    }
 
 }
