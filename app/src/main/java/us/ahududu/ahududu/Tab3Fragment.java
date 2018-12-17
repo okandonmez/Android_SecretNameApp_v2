563ad90c4da9f99960689a9b4c2848c8e67e774b
apple <donmezo17@itu.edu.tr>
2018-12-18 00:50:51 +0300

    Dudula Page Performance Problem Fixed

--- app/src/main/java/us/ahududu/ahududu/Tab3Fragment.java
+++ app/src/main/java/us/ahududu/ahududu/Tab3Fragment.java
@@ -5,28 +5,29 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.CardView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.RelativeLayout;
 
-public class Tab3Fragment extends Fragment {
+public class Tab3Fragment extends Fragment implements View.OnClickListener{
     private static final String TAG = "tab3Fragment";
-    View vwQuick, vwLocation, vwEvent, vwAdvanced;
+    CardView vwQuick, vwLocation, vwEvent, vwAdvanced;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.tab3_fragment,container,false);
-       // connectUI(view);
+        connectUI(view);
         return view;
     }
-/*
+
     private void connectUI(View view){
-        vwQuick = view.findViewById(R.id.dudulaQuickEvent);
-        vwLocation = view.findViewById(R.id.dudulaSearchByLocation);
-        vwEvent = view.findViewById(R.id.dudulaEvent);
-        vwAdvanced = view.findViewById(R.id.dudulaSearchAdvanced);
+        vwQuick = view.findViewById(R.id.cvQuick);
+        vwLocation = view.findViewById(R.id.cvLocation);
+        vwEvent = view.findViewById(R.id.cvEvent);
+        vwAdvanced = view.findViewById(R.id.cvAdcanced);
 
         vwQuick.setOnClickListener(this);
         vwLocation.setOnClickListener(this);
@@ -38,20 +39,20 @@ public class Tab3Fragment extends Fragment {
     public void onClick(View view) {
         int id = view.getId();
         switch (id){
-            case R.id.dudulaQuickEvent:
+            case R.id.cvQuick:
                 Intent intent2 = new Intent(getActivity().getApplicationContext(), AddQuickEvent.class);
                 startActivity(intent2);
                 break;
-            case R.id.dudulaSearchByLocation:
+            case R.id.cvLocation:
                 // Do sth
                 break;
-            case R.id.dudulaEvent:
+            case R.id.cvEvent:
                 Intent intent = new Intent(getActivity().getApplicationContext(),addEvents.class);
                 startActivity(intent);
                 break;
-            case R.id.dudulaSearchAdvanced:
+            case R.id.cvAdcanced:
                 // Do sth
                 break;
         }
-    } */
+    }
 }
