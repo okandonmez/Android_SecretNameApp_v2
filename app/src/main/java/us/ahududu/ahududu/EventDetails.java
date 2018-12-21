8fa690e17479b190f16c72a0e902e5a50a550a98
Okan Dönmez <ce.okandonmez@gmail.com>
2018-12-21 16:34:55 +0300

    Sponsored details, Event details added

--- app/src/main/java/us/ahududu/ahududu/EventDetails.java
+++ app/src/main/java/us/ahududu/ahududu/EventDetails.java
@@ -7,15 +7,33 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class EventDetails extends AppCompatActivity {
 
-    String token;
+    String strToken;
     String eventId;
+    String eventDetailsURL = "http://31.210.91.130/api/Activity/GetSpecActDetails?id=";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_event_details);
         getToken();
+        getEventId();
+        getEventDetails();
+    }
+
+    private void getEventId(){
         Bundle extras = getIntent().getExtras();
         eventId = extras.getString("eventId");
         Log.e("eventId", eventId);
@@ -23,6 +41,30 @@ public class EventDetails extends AppCompatActivity {
 
     private void getToken(){
         SharedPreferences settings = getApplicationContext().getSharedPreferences("tokenizer", Context.MODE_PRIVATE);
-        token = settings.getString("access_token", "no_token");
+        strToken = settings.getString("access_token", "no_token");
+    }
+
+    private void getEventDetails(){
+        RequestQueue requestQueue = Volley.newRequestQueue(getApplicationContext());
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, eventDetailsURL + eventId , new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                Log.e("GetEventsResponse", response);
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.e("GetEventsErrorResponse", error.toString());
+            }
+        }){
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                Map<String, String> param = new HashMap<String, String>();
+                param.put("Authorization","Bearer " + strToken);
+                return param;
+            }
+        };
+
+        requestQueue.add(stringRequest);
     }
 }
--- app/src/main/java/us/ahududu/ahududu/SponsoredDetails.java
+++ app/src/main/java/us/ahududu/ahududu/SponsoredDetails.java
@@ -1,18 +1,71 @@
 package us.ahududu.ahududu;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.Map;
+
 public class SponsoredDetails extends AppCompatActivity {
+
+    String sponsoredURL = "http://31.210.91.130/api/Sponsored/GetSponsoredEventsDetails?id=";
     int eventId;
+    String strToken;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sponsored_details);
+        getEventsId();
+        getToken();
+        getEventDetails();
+    }
 
+    private void getEventsId(){
         Bundle extras = getIntent().getExtras();
         eventId = extras.getInt("eventId");
         Toast.makeText(getApplicationContext(),eventId+"",Toast.LENGTH_LONG).show();
+    }
+
+    private void getToken(){
+        SharedPreferences settings = getApplicationContext().getSharedPreferences("tokenizer", Context.MODE_PRIVATE);
+        strToken = settings.getString("access_token", "no_token");
+    }
+
+    private void getEventDetails(){
+        RequestQueue requestQueue = Volley.newRequestQueue(getApplicationContext());
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, sponsoredURL + Integer.toString(eventId) , new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                Log.e("GetEventsResponse", response);
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.e("GetEventsErrorResponse", error.toString());
+            }
+        }){
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                Map<String, String> param = new HashMap<String, String>();
+                param.put("Authorization","Bearer " + strToken);
+                return param;
+            }
+        };
+
+        requestQueue.add(stringRequest);
     }
 }
